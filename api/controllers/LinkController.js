2d4a7557e0cde4c8922291033aff93745cde31fc
MisterCh0c <wojhack@unseen.is>
2015-04-07 19:17:53 +0200

    Form fonctionel

--- api/controllers/LinkController.js
+++ api/controllers/LinkController.js
@@ -7,29 +7,28 @@ module.exports = {
 
     },
 
-    create: function(req,res){
-    	var params=req.allParams();
-    	//Link.create({title:params.title,link:params.link,description:params.description,
-    	//	lang:params.lang,rank})
-		console.log(params.link);
-		var rank=getAlexaRank(params.link); //undefined
-		console.log('ransqsqsk = '+rank);
+    create: function(req, res) {
+        var params = req.allParams();
+        //getAlexaRank(params.link, req);
 
+        alexa(params.link, function(error, rk) {
+
+            Link.create({
+                title: params.title,
+                link: params.link,
+                description: params.description,
+                lang: params.lang,
+                type: params.type,
+                rank: rk.rank,
+            }).exec(function(e, r) {
+                console.log(r);
+            });
+            //console.log(params.link);
+
+            //console.log('ransqsqsk = ' + req.rank);
+        });
     },
 
 };
 
 //########################## END CONTROLLER ################################
-
-function getAlexaRank(url){
-
-    alexa(url, function(error, result) {
-        if (!error) {
-            console.log(JSON.stringify(result));
-        } else {
-            console.log(error);
-        }
-        return result;
-    });
-
-}
--- api/models/Link.js
+++ api/models/Link.js
@@ -14,6 +14,7 @@ module.exports = {
   	description:'string',
   	rank:'string',
   	lang:'string',
+  	type: 'string'
   }
 };
 
