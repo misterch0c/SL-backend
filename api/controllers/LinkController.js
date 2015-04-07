922b4c12acbadd91dec478f2b4c209529b8aaf91
MisterCh0c <wojhack@unseen.is>
2015-04-07 18:21:57 +0200

    Rank issue

--- api/controllers/LinkController.js
+++ api/controllers/LinkController.js
@@ -1,23 +1,35 @@
-/**
- * LinkController
- *
- * @description :: Server-side logic for managing links
- * @help        :: See http://links.sailsjs.org/docs/controllers
- */
-
-module.exports = {
-	
-test : function(req,res){
 var alexa = require('alexarank');
 
-alexa("http://www.zenk-security.com/", function(error, result) {
-    if (!error) {
-        console.log(JSON.stringify(result));
-    } else {
-        console.log(error);
-    }
-});
-}
+
+module.exports = {
+
+    test: function(req, res) {
+
+    },
+
+    create: function(req,res){
+    	var params=req.allParams();
+    	//Link.create({title:params.title,link:params.link,description:params.description,
+    	//	lang:params.lang,rank})
+		console.log(params.link);
+		var rank=getAlexaRank(params.link); //undefined
+		console.log('rank = ',rank);
+
+    },
 
 };
 
+//########################## END CONTROLLER ################################
+
+function getAlexaRank(url){
+
+    alexa(url, function(error, result) {
+        if (!error) {
+            console.log(JSON.stringify(result));
+        } else {
+            console.log(error);
+        }
+        return result;
+    });
+
+}
