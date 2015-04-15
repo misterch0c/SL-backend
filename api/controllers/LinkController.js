39490253e67ff6c053dfc4bf1b446c0101031ae1
MisterCh0c <wojhack@unseen.is>
2015-04-15 11:35:50 +0200

    Website preview

--- api/controllers/LinkController.js
+++ api/controllers/LinkController.js
@@ -1,5 +1,7 @@
 var alexa = require('alexarank');
 var metadata = require('web-metadata');
+var phantom = require('phantom');
+var webpagePreview = require('webpage-preview');
 
 module.exports = {
 
@@ -28,32 +30,42 @@ module.exports = {
             //console.log('ransqsqsk = ' + req.rank);
         });
     },
-	
-	getDesc: function(req, res){		
-		
-		var link = req.param('link');
-		
-		console.log('getDesc : ' + link);
-		var opts = {
-			url: link
-		};
-		metadata(opts, function(err, data) {	
-			var desc;		
-			if (err === null){
-				console.log(data.title);
-				console.log(err);
-				if (typeof data.meta.description !== "undefined"){
-					console.log(data.meta.description);			
-					desc = data.meta.description;
-				}
-				else {
-					console.log(data.meta.DESCRIPTION);			
-					desc = data.meta.DESCRIPTION;				
-				}
-			}
-			return res.json(200,desc);
-		});
-	}
+
+
+    test: function(req, res) {
+        webpagePreview.generatePreview('http://www.zenk-security.com/', 'google', '/home/unkn0wn/genpic' + '/public/previews', null, null, function(error, sizePaths) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(sizePaths);
+            }
+        });
+    },
+
+    getDesc: function(req, res) {
+
+        var link = req.param('link');
+
+        console.log('getDesc : ' + link);
+        var opts = {
+            url: link
+        };
+        metadata(opts, function(err, data) {
+            var desc;
+            if (err === null) {
+                console.log(data.title);
+                console.log(err);
+                if (typeof data.meta.description !== "undefined") {
+                    console.log(data.meta.description);
+                    desc = data.meta.description;
+                } else {
+                    console.log(data.meta.DESCRIPTION);
+                    desc = data.meta.DESCRIPTION;
+                }
+            }
+            return res.json(200, desc);
+        });
+    }
 
 };
 
