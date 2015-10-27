494ad5885cd0746f27b861fb678e18b08ec3faa8
misterCh0c <you@example.com>
2015-10-27 22:41:50 +0100

    up

--- api/services/base.js
+++ api/services/base.js
@@ -4,7 +4,7 @@ var request = require('request');
 var parse = require('xml-parser');
 var isUp = require('is-up');
 var metadata = require('web-metadata');
-var phantom = require ('phantom');
+var phantom = require('phantom');
 var WebPage = require('webpage');
 var screenshot = require('url-to-screenshot');
 var webshot = require('webshot');
@@ -18,66 +18,64 @@ module.exports = {
         console.log(options);
 
         var params = options;
-        pathArray=params.link.split('/');
+        console.log(params);
+        pathArray = params.link.split('/');
         protocol = pathArray[0];
         host = pathArray[2];
         url = protocol + '//' + host;
-        url=url.replace(/.*?:\/\//g, "");//Strip http,https
+        url = url.replace(/.*?:\/\//g, ""); //Strip http,https
         console.log(url);
         //var rawLink=params.link.replace(/.*?:\/\//g, "");//Strip http,https
         //rawLink=rawLink.replace(/\/$/, ""); // Strip trailing slash
-       //console.log('RAAAAAAAAAAAw'+rawLink);
+        //console.log('RAAAAAAAAAAAw'+rawLink);
         console.log(params.title);
 
-            console.log('-----------------');
-            console.log(params.link);
+        console.log('-----------------');
+        console.log(params.link);
 
 
-            var renderStream = webshot(params.link);
-            var file = fs.createWriteStream('assets/tt/'+params.title+'.png', {encoding: 'binary'});
+        // var renderStream = webshot(params.link);
+        // var file = fs.createWriteStream('assets/screenshots/' + params.title + '.png', {
+        //     encoding: 'binary'
+        // });
 
-            renderStream.on('data', function(data) {
-              file.write(data.toString('binary'), 'binary');
-              console.log("WRITTEEEEEEEEEEEEN");
-            });
-
-
-            getAlexa(params.link, function(jayz) {
-
-
-                
-                isUp(url, function(err, up) {
-                    getDescTitle(params.link,function(rez){
-                        //console.log("cbbbbbbbbb");
-                        //console.log(rez);
+        // renderStream.on('data', function(data) {
+        //     file.write(data.toString('binary'), 'binary');
+        //     console.log("WRITTEEEEEEEEEEEEN");
+        // });
+        getAlexa(params.link, function(jayz) {
 
 
-                        Link.create({
-                            title: params.title,
-                            link: params.link,
-                            description: rez.description,
-                            lang: params.lang,
-                            type: params.type,
-                            rank: jayz.rank,
-                            delta: jayz.delta,
-                            isup: up,
-                        }).exec(function(e, r) {
-                            console.log("created this");
-                            console.log(r);
-                        });
+            isUp(url, function(err, up) {
+                getDescTitle(params.link, function(rez) {
+                    //console.log("cbbbbbbbbb");
+                    //console.log(rez);
+                    getPreview(params.link, params.title);
 
+                    Link.create({
+                        title: params.title,
+                        link: params.link,
+                        description: rez.description,
+                        lang: params.lang,
+                        type: params.type,
+                        rank: jayz.rank,
+                        delta: jayz.delta,
+                        isup: up,
+                    }).exec(function(e, r) {
+                        console.log("created this");
+                        console.log(r);
                     });
+
                 });
             });
+        });
 
 
     },
 
 
     getDesc: function(options) {
-        
         var link = options;
-
         console.log('getDesc : ' + link);
         var opts = {
             url: link
@@ -97,37 +95,22 @@ module.exports = {
                     desc = data.meta.DESCRIPTION;
                 }
             }
-            //console.log(desc);
+
             return desc;
         });
     },
 };
 
 function getPreview(url, title) {
-    //need to generate unique title here otherwise we'll get pwnd 
-    // webpagePreview.generatePreview('http://google.com', title, '/home/unkn0wn/web/SL-backend/assets/prev/', null, {
-    //     small: {
-    //         width: 160,
-    //         height: 120
-    //     }
-    // }, function(error, sizePaths) {
-    //     if (error) {
-    //         console.log(error);
-    //     } else {
-    //         console.log(sizePaths);
-    //         console.log("hey");
+    var renderStream = webshot(url);
+    var file = fs.createWriteStream('assets/screenshots/' + title + '.png', {
+        encoding: 'binary'
+    });
 
-    //     }
-    //         prevCB(sizePaths);
-
-    // });
-var renderStream = webshot(url);
-var file = fs.createWriteStream('assets/tt/'+title+'.png', {encoding: 'binary'});
-
-renderStream.on('data', function(data) {
-  file.write(data.toString('binary'), 'binary');
-  console.log("WRITTEEEEEEEEEEEEN");
-});
+    renderStream.on('data', function(data) {
+        file.write(data.toString('binary'), 'binary');
+        console.log("< --- Screenshot written --- >");
+    });
 }
 
 function getAlexa(url, alexaCB) {
@@ -137,34 +120,34 @@ function getAlexa(url, alexaCB) {
         if (!error && response.statusCode == 200) {
             var obj = parse(body);
             var ch;
-            for (var i =0; i<obj.root.children.length;++i){
-                if(obj.root.children[i].name=='SD'){
+            for (var i = 0; i < obj.root.children.length; ++i) {
+                if (obj.root.children[i].name == 'SD') {
                     ch = obj.root.children[i];
                 }
             }
 
             //console.log(ch.children[1].attributes.RANK);
-           // console.log(ch.children);
+            // console.log(ch.children);
             //console.log(ch.children[2].attributes.DELTA);
             //var rank = ch.children[1].attributes.RANK;
             console.log(ch);
-            if (typeof ch !== 'undefined'){
-                if (typeof ch.children[2] !== 'undefined'){
-                    
-                    var delta = ch.children[2].attributes.DELTA;
-                }else{
-                    var delta="no";
-                }            
+            if (typeof ch !== 'undefined') {
+                if (typeof ch.children[2] !== 'undefined') {
 
-                if (typeof ch.children[1] !== 'undefined'){
-                    
-                    var rank = ch.children[1].attributes.RANK;
-                }else{
-                    var rank="no";
+                    var delta = ch.children[2].attributes.DELTA;
+                } else {
+                    var delta = "no";
                 }
-                        
+
+                if (typeof ch.children[1] !== 'undefined') {
+
+                    var rank = ch.children[1].attributes.RANK;
+                } else {
+                    var rank = "no";
+                }
+
             }
-            
+
             var jayz = {
                 rank: rank,
                 delta: delta
@@ -175,37 +158,38 @@ function getAlexa(url, alexaCB) {
     });
 }
 
-function getDescTitle(url,descCB){
-        var link = url;
-        var desc;
-        var title;
+function getDescTitle(url, descCB) {
+    var link = url;
+    var desc;
+    var title;
 
-        console.log('getDesc : ' + link);
-        var opts = {
-            url: link
-        };
-        metadata(opts, function(err, data) {
-            console.log(data);
-            if (err === null) {
-                console.log(data.title);
-                console.log(err);
-                if (typeof data.meta.description !== "undefined") {
-                    console.log(data.meta.description);
+    console.log('getDesc : ' + link);
+    var opts = {
+        url: link
+    };
+    metadata(opts, function(err, data) {
+        console.log(data);
+        if (err === null) {
+            console.log(data.title);
+            console.log(err);
+            if (typeof data.meta.description !== "undefined") {
+                console.log(data.meta.description);
 
-                    desc = data.meta.description;
-                    title=data.title
-                } else {
-                    console.log(data.meta.DESCRIPTION);
-                   
-                        desc=data.meta.DESCRIPTION
-                        title=data.title
-                }
+                desc = data.meta.description;
+                title = data.title
+            } else {
+                console.log(data.meta.DESCRIPTION);
 
-                var rez={title:title,
-                    description:desc
-                }
-                descCB(rez);
+                desc = data.meta.DESCRIPTION
+                title = data.title
             }
 
-        });
+            var rez = {
+                title: title,
+                description: desc
+            }
+            descCB(rez);
+        }
+
+    });
 }
